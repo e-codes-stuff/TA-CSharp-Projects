51cbc38ed5f39f75c0e4ba35b394a53372cdce11
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2024-03-20 11:10:32 -0700

    Update DailyReport

--- DailyReport/DailyReportProgram/Program.cs
+++ DailyReport/DailyReportProgram/Program.cs
@@ -1,39 +1,27 @@
-﻿// Asks a question to the user
-string ask_question(string question)
-{
-    Console.WriteLine(question);
-    var answer = Console.ReadLine();
-
-    // If there is no answer given (null or empty response)
-    // ask again until one is entered
-    while (answer == null || answer == "")
-    {
-        Console.WriteLine("Please answer the prompt:");
-        Console.WriteLine(question);
-
-        // Set answer to new input
-        answer = Console.ReadLine();
-    }
-
-    return answer;
-}
-
-Console.WriteLine("The Tech Academy\nStudent Daily Report");
+﻿Console.WriteLine("The Tech Academy\nStudent Daily Report");
 
 // Ask questions
-var name = ask_question("What is your name?");
-var course = ask_question("What course are you on?");
-var page_number = ask_question("What page number are you on?");
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
 
-// Ask if help is needed and ensure answer provided is true or false
-var help_needed = ask_question("Do you need help with anything? Please answer \"true\" or \"false\"");
-while (help_needed != "true" && help_needed != "false")
-{
-    help_needed = ask_question("Do you need help with anything? Please answer \"true\" or \"false\"");
-}
+Console.WriteLine("What course are you on?");
+var course = Console.ReadLine();
 
-var positive_experiences = ask_question("Were there any positive experiences you’d like to share? Please give specifics.");
-var other_feedback = ask_question("Is there any other feedback you’d like to provide? Please be specific.");
-var hours_studied = ask_question("How many hours did you study today?");
+// Ask for page number and convert to Int32
+Console.WriteLine("What page number are you on?");
+var page_number = Convert.ToInt32(Console.ReadLine());
+
+// Ask if help is needed and convert answer to bool
+Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+var help_needed = Convert.ToBoolean(Console.ReadLine());
+
+Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+var positive_experiences = Console.ReadLine();
+
+Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+var other_feedback = Console.ReadLine();
+
+Console.WriteLine("How many hours did you study today?");
+var hours_studied = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
