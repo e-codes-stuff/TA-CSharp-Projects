8f095ef96393d49b0af951901e730ed0d48e8cb4
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2024-04-03 10:33:52 -0700

    Add MethodCalls

---
+++ MethodCalls/MethodCalls/MathOperations.cs
@@ -0,0 +1,7 @@
+// Define a new class with multiple math operations
+class MathOperations
+{
+    public int AddTen(int number) => number + 10;
+    public int MultiplyByTwo(int number) => number * 2;
+    public int SubtractFive(int number) => number - 5;
+}
---
+++ MethodCalls/MethodCalls/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter a number to perform math operations on:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+// Create an instance of MathOperations
+MathOperations operations = new MathOperations();
+
+// Call the AddTen method
+int resultAddTen = operations.AddTen(userNumber);
+Console.WriteLine($"Adding 10 to your number: {resultAddTen}");
+
+// Call the MultiplyByTwo method
+int resultMultiplyByTwo = operations.MultiplyByTwo(userNumber);
+Console.WriteLine($"Multiplying your number by 2: {resultMultiplyByTwo}");
+
+// Call the SubtractFive method
+int resultSubtractFive = operations.SubtractFive(userNumber);
+Console.WriteLine($"Subtracting 5 from your number: {resultSubtractFive}");
