36925009ff0a85ebbfeb09046d04d79f42efc7ef
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2024-04-12 14:27:47 -0700

    Add ParsingEnums

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,31 @@
+﻿// Define an enum for the days of the week
+enum DayOfWeek
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the current day of the week:");
+
+        try
+        {
+            // Assign the value to a variable of the DayOfWeek enum data type
+            DayOfWeek currentDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine(), true);
+            Console.WriteLine($"You entered: {currentDay}");
+        }
+        catch
+        {
+            // If an error occurs, print this message to the console
+            Console.WriteLine("Please enter an actual day of the week.");
+        }
+    }
+}
