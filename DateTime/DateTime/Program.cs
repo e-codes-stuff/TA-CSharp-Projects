1107194330586fcb157f83be153e800e236dc1b1
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2024-04-19 11:43:52 -0700

    Add DateTime

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DateTime
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console
+            Console.WriteLine("Current date and time: " + DateTime.Now.ToString());
+
+            // Ask the user for a number
+            Console.Write("Please enter a number of hours: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate and print the exact time it will be in X hours
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine($"In {hours} hours, it will be: {futureTime}");
+        }
+    }
+
+}
