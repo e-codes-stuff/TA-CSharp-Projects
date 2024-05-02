5571b4b60f7e9eb4f7e8519a08f1d9f7f3b83588
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2024-05-02 12:42:52 -0700

    Add ASPWebApp

---
+++ ASPWebApp/ASPWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
